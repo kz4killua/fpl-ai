0600982a9ddfdf6bb6cd3a16a1ba5f00051bb00c
Ifeanyi Obinelo <62487964+KZ4Killua@users.noreply.github.com>
2022-10-02 15:20:13 +0100

    Update data gathering for new model

--- data.py
+++ data.py
@@ -1,18 +1,18 @@
 import os
 import subprocess
 import json
-import csv
-import difflib
 
 import pandas as pd
-import numpy as np
 
 import api
-import understat
 
 
 DATE_FORMAT = r'%Y-%m-%dT%H:%M:%SZ'
-ROLLING_STATISTICS = ['total_points']
+ROLLING_STATISTICS = (
+    'total_points', 'influence', 'creativity', 'threat', 
+    'ict_index', 'was_home', 'goals_scored', 'goals_conceded', 
+    'assists', 'clean_sheets', 'bonus', 'bps', 'minutes')
+ROLLING_WINDOWS = (3, 40)
 
 
 def year_to_season(year):
@@ -32,7 +32,7 @@ def get_last_checked_event(events):
 
 def get_current_season(fixtures):
     """Returns the current season."""
-    year = int(fixtures.iloc[0]['kickoff_time'].split('-')[0])
+    year = int(fixtures.dropna().iloc[0]['kickoff_time'].split('-')[0])
     return year_to_season(year)
 
 
@@ -78,272 +78,60 @@ def update_players_data(season, elements, events):
         json.dump(last_checked, f)
 
 
-def get_player_current_data(df, next_gameweek):
-    """Extracts a player's stats before a given gameweek."""
+def get_current_data(season, elements, next_gameweek):
+    """Fetches current player data for making predictions."""
 
-    # Filter the data to events before the given gameweek.
-    df = df[df['round'] < next_gameweek].copy().reset_index()
+    elements = elements.set_index('id', drop=False)
 
-    if len(df) == 0:
-        return None
+    # Load player data for the previous season
+    previous_season = year_to_season(season_to_year(season) - 1)
+    previous = pd.read_csv(f'data/api/merged/players/{previous_season}.csv')
+    previous['season'] = previous_season
 
-    # Convert the kickoff times to date objects.
-    df['kickoff_time'] = pd.to_datetime(df['kickoff_time'], format=DATE_FORMAT)
-    # Sort rows by dates.
-    df.sort_values('kickoff_time', ignore_index=True, inplace=True)
-
-    # We will store player data in a series.
-    data = pd.Series(dtype='float64')
-
-    # Get the player id.
-    data.loc['id'] = df.loc[0, 'element']
-
-    # Compute rolling statistics.
-    for column in ROLLING_STATISTICS:
-        data.loc['avg_' + column + '_5_games'] = df[column].iloc[-5:].mean()
-        data.loc['avg_' + column] = df[column].mean()
-
-    # Check if the player got a red card in their last match.
-    data.loc['is_suspended'] = df.iloc[-1]['red_cards']
-
-    return data
-
-
-def get_all_current_data(season, elements, teams, next_gameweek):
-
-    data = []
-    # Read data for each player
+    # Load player data for the current season
+    current = list()
     for item in os.scandir(f'data/api/{season}/players'):
         if item.path.endswith('.csv'):
-            df = pd.read_csv(item.path)
-            player_data = get_player_current_data(df, next_gameweek)
-            if player_data is None:
+            try:
+                player = pd.read_csv(item.path)
+            except pd.errors.EmptyDataError:
                 continue
-            data.append(player_data)
-
-    df = pd.DataFrame(data)
-    # Map other information.
-    df['element_type'] = df['id'].map(elements.set_index('id')['element_type'])
-    df['team'] = df['id'].map(elements.set_index('id')['team'])
-
-    return df
-
-
-def calculate_similarity(s1: str, s2: str):
-    """Returns a score indicating how similar two strings are."""
-    # Convert both strings to lowercase
-    s1 = s1.lower().strip()
-    s2 = s2.lower().strip()
-    # Match with difflib
-    matcher = difflib.SequenceMatcher(None, s1, s2)
-    return matcher.ratio()
-
-
-def map_closest_names(a: dict, b: dict):
-    """
-    Maps each name in a.values() with the most similar name in b.values()
-    """
-    output = dict()
-    # Get all possible pairs
-    pairs = [(k1, k2) for k1 in a.keys() for k2 in b.keys()]
-    # Rank the pairs by similarity
-    pairs.sort(key = lambda pair: calculate_similarity(a[pair[0]], b[pair[1]]), reverse=True)
-    # Map each key in a to the closest key in b
-    mapped_a = set()
-    mapped_b = set()
-
-    for pair in pairs:
-        if not(pair[0] in mapped_a or pair[1] in mapped_b):
-            # Add the mapping
-            output[pair[0]] = pair[1]
-            # Keep track of what we have already mapped
-            mapped_a.add(pair[0])
-            mapped_b.add(pair[1])
-
-    return output
-
-
-def update_player_ids(season):
-    """Update FPL to understat mappings for player ids."""
-
-    # Gather players data from the FPL API
-    fpl_players = pd.read_csv(f'data/vaastav/data/{season}/players_raw.csv')
-    # Gather players data from the understat API
-    understat_players = understat.get_league_players_data('EPL', season_to_year(season))
-    understat_players = pd.DataFrame(understat_players)
-    understat_players['id'] = understat_players['id'].astype('int')
-    # Load IDs
-    player_ids = pd.read_csv('data/understat/player_ids.csv')
-
-    # Get all FPL players who have not been assigned understat ids
-    unmatched_fpl = set(fpl_players['code'].values) - set(player_ids['fpl_code'].values)
-    # Filter out all those that have not played a match (they may not have understat data)
-    unmatched_fpl &= set(fpl_players[fpl_players['minutes'] != 0]['code'].values)
-    # Match their codes to their full names
-    unmatched_fpl = dict(
-        fpl_players.set_index('code').loc[list(unmatched_fpl)]['first_name'] +
-        ' ' +
-        fpl_players.set_index('code').loc[list(unmatched_fpl)]['second_name'])
-    # Add web names
-    for code, full_name in unmatched_fpl.items():
-        web_name = fpl_players.set_index('code').loc[code]['web_name']
-        if web_name not in full_name:
-            unmatched_fpl[code] += ' ' + web_name
-
-    # Get all understat players who have not been assigned FPL codes
-    unmatched_understat = understat_players[
-        ~understat_players['id'].isin(player_ids['understat_id'].values)
-        ]
-    # Map their ids to their names
-    unmatched_understat = dict(unmatched_understat.set_index('id')['player_name'])
-
-    # Map names
-    closest_names = map_closest_names(unmatched_fpl, unmatched_understat)
-
-    with open("data/understat/player_ids.csv", 'a', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        for pair in closest_names.items():
-            fpl_code, understat_id = pair
-            fpl_name = unmatched_fpl[fpl_code]
-            understat_name = unmatched_understat[understat_id]
-            writer.writerow([fpl_code, understat_id, fpl_name, understat_name])
-
-
-def update_team_ids(season):
-    """Update FPL to understat mappings for team ids."""
-
-    # Get FPL team data
-    fpl_teams = pd.read_csv(f'data/vaastav/data/{season}/teams.csv')
-    # Get understat team data
-    understat_teams = understat.get_league_teams_data('EPL', season_to_year(season))
-    understat_teams = pd.DataFrame(understat_teams.values())
-    understat_teams.drop('history', axis=1, inplace=True)
-    understat_teams['id'] = understat_teams['id'].astype('int')
-    # Load team IDs
-    team_ids = pd.read_csv('data/understat/team_ids.csv')
-
-    # Get all FPL teams who have not been mapped
-    unmatched_fpl = set(fpl_teams['code'].values) - set(team_ids['fpl_code'].values)
-    # Match their codes to their full names
-    unmatched_fpl = dict(
-        fpl_teams.set_index('code').loc[list(unmatched_fpl)]['name']
-        )
-
-    # Get all understat teams that have not been assigned FPL codes
-    unmatched_understat = understat_teams[
-        ~understat_teams['id'].isin(team_ids['understat_id'].values)
-        ]
-    # Map their ids to their names
-    unmatched_understat = dict(unmatched_understat.set_index('id')['title'])
-
-    # Map names
-    closest_names = map_closest_names(unmatched_fpl, unmatched_understat)
-
-    with open("data/understat/team_ids.csv", 'a', encoding='utf-8') as f:
-        writer = csv.writer(f)
-        for pair in closest_names.items():
-            fpl_code, understat_id = pair
-            fpl_name = unmatched_fpl[fpl_code]
-            understat_name = unmatched_understat[understat_id]
-            writer.writerow([fpl_code, understat_id, fpl_name, understat_name])
+            player_id = int(item.name[:-4])
+            if player_id in elements['id']:
+                player['code'] = elements.loc[player_id, 'code']
+                current.append(player)
+    current = pd.concat(current)
+    current['season'] = season
     
+    # Filter out unnecessary records
+    current = current[current['round'] < next_gameweek]
 
-def update_fixture_ids(season):
-    """Update FPL to understat mappings for fixture ids."""
+    # Combine both dataframes
+    players = pd.concat([previous, current])
 
-    # Load FPL fixtures for that season
-    fpl_fixtures = pd.read_csv(f'data/vaastav/data/{season}/fixtures.csv')
+    # Sort player records by kickoff times
+    players['kickoff_time'] = pd.to_datetime(players['kickoff_time'], format=DATE_FORMAT)
+    players.sort_values('kickoff_time', ignore_index=True, inplace=True)
 
-    # Get all understat fixtures for that season
-    understat_fixtures = understat.get_league_dates_data("EPL", season_to_year(season))
+    data = list()
+    for index, code, id in elements[['code', 'id']].itertuples():
+        player = dict()
+        # Filter out each player's records
+        records = players[players['code'] == code]
+        # Compute rolling stats for each player
+        for feature in ROLLING_STATISTICS:
+            for window in ROLLING_WINDOWS:
+                player[f'avg_{feature}_{window}_games'] = records[feature].iloc[-window:].mean() if len(records) else 0
+        # Add other player stats
+        player['id'] = id
+        player['team'] = elements.loc[id, 'team']
+        player['is_gkp'] = 1 if elements.loc[id, 'element_type'] == 1 else 0
+        player['is_def'] = 1 if elements.loc[id, 'element_type'] == 2 else 0
+        player['is_mid'] = 1 if elements.loc[id, 'element_type'] == 3 else 0
+        player['is_fwd'] = 1 if elements.loc[id, 'element_type'] == 4 else 0
+        # Add to the list
+        data.append(player)
 
-    # Get home and away ids
-    for fixture in understat_fixtures:
-        fixture['h'] = fixture['h']['id']
-        fixture['a'] = fixture['a']['id']
+    data = pd.DataFrame(data)
 
-    # Make a data frame for understat fixtures
-    understat_fixtures = pd.DataFrame(understat_fixtures)
-
-    # Convert data types
-    understat_fixtures['id'] = understat_fixtures['id'].astype('int')
-    understat_fixtures['h'] = understat_fixtures['h'].astype('int')
-    understat_fixtures['a'] = understat_fixtures['a'].astype('int')
-
-    # Load team ids
-    team_ids = pd.read_csv('data/understat/team_ids.csv')
-    # Load team data for that season
-    teams = pd.read_csv(f'data/vaastav/data/{season}/teams.csv')
-
-    # Map fpl codes for home and away teams
-    fpl_fixtures['team_h_fpl_code'] = fpl_fixtures['team_h'].map(teams.set_index('id')['code'])
-    fpl_fixtures['team_a_fpl_code'] = fpl_fixtures['team_a'].map(teams.set_index('id')['code'])
-    # Do the same for understat
-    understat_fixtures['team_h_fpl_code'] = understat_fixtures['h'].map(team_ids.set_index('understat_id')['fpl_code'])
-    understat_fixtures['team_a_fpl_code'] = understat_fixtures['a'].map(team_ids.set_index('understat_id')['fpl_code'])
-
-    understat_fixtures.set_index(['team_h_fpl_code', 'team_a_fpl_code'], inplace=True)
-    fpl_fixtures.set_index(['team_h_fpl_code', 'team_a_fpl_code'], inplace=True)
-
-    # Now, match each understat fixture to its FPL code
-    understat_fixtures['fpl_id'] = understat_fixtures.index.map(fpl_fixtures['id'])
-
-    # Cleanup
-    understat_fixtures.reset_index(inplace=True)
-    understat_fixtures.rename({'id': 'understat_id'}, axis=1, inplace=True)
-
-    # Get the columns we need
-    fixture_ids = understat_fixtures
-
-    fixture_ids['h_fpl'] = fixture_ids['team_h_fpl_code'].map(
-        team_ids.set_index('fpl_code')['fpl_name'])
-    fixture_ids['a_fpl'] = fixture_ids['team_a_fpl_code'].map(
-        team_ids.set_index('fpl_code')['fpl_name'])
-
-    fixture_ids['h_understat'] = fixture_ids['h'].map(
-        team_ids.set_index('understat_id')['understat_name'])
-    fixture_ids['a_understat'] = fixture_ids['a'].map(
-        team_ids.set_index('understat_id')['understat_name'])
-
-    fixture_ids = fixture_ids[[
-        'fpl_id', 'understat_id', 'h_fpl', 'a_fpl', 
-        'h_understat', 'a_understat']]
-
-    # Save
-    fixture_ids.to_csv(f'data/understat/season/{season_to_year(season)}/fixture_ids.csv', index=False)
-
-
-def save_understat_player_matches_data(season):
-    """Saves understat data for all players who played in a season"""
-
-    # Get ids of all understat players in that league
-    players_data = understat.get_league_players_data('EPL', season_to_year(season))
-    understat_ids = pd.DataFrame(players_data)['id'].astype('int').values
-
-    for id in understat_ids:
-        player_matches_data = understat.get_player_matches_data(id)
-        # Save to a CSV
-        player_matches_data = pd.DataFrame(player_matches_data)
-        player_matches_data.to_csv(f'data/understat/player/matches/{id}.csv', index=False)
-
-
-def save_understat_league_teams_data(season):
-    # Get all the data
-    teams_data = understat.get_league_teams_data('EPL', season_to_year(season))
-
-    for team in teams_data.values():
-        # Create dataframe
-        history = pd.DataFrame(team['history'])
-        history['id'] = team['id']
-        history['title'] = team['title']
-        # Save
-        history.to_csv(f"data/understat/season/{season_to_year(season)}/teams/{team['id']}.csv", index=False)
-
-
-
-
-def main():
-    pass
-
-if __name__ == '__main__':
-    main()
+    return data
