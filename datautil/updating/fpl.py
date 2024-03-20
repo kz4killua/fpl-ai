501490dce52d7803006adeb34348c9827794f856
Ifeanyi Obinelo <ifeanyiobinelo@outlook.com>
2024-03-20 00:46:16 -0400

    Update data only on new gameweeks.

--- datautil/updating/fpl.py
+++ datautil/updating/fpl.py
@@ -1,47 +1,19 @@
 """Functions for updating local Fantasy Premier League data."""
 
-from pathlib import Path
-import json
-
 import pandas as pd
 from tqdm import tqdm
 
-from api.fpl import get_player_data, get_fixture_data, get_bootstrap_data
+from api.fpl import get_player_data, get_fixture_data
 from datautil.constants import LOCAL_DATA_PATH
 
 
-def update_local_players(season, elements, events):
-    """Update Fantasy Premier League data for players."""
-
-    # Check the most recent gameweek
-    last_updated_gameweek = int(events[events['data_checked'] == True]['id'].max())
-
-    # Check if the local data is already up to date
-    checkpoint = LOCAL_DATA_PATH / f"api/{season}/local_players_last_update.json"
-
-    if checkpoint.exists():
-        with open(checkpoint, 'r') as f:
-            if last_updated_gameweek == json.load(f):
-                return
-    else:
-        checkpoint.parent.mkdir(parents=True, exist_ok=True)
-
-    # Save each player's data
-    for element in tqdm(elements['id'].unique(), desc="Updating local players"):
-        
-        # Get the player's season summary
+def update_local_players(season, elements):
+    """Update Fantasy Premier League data for each player."""
+    for element in tqdm(elements['id'].unique(), desc="Updating local players"):        
         data = get_player_data(element)['history']
-
-        # Create the folder to store the data
         path = LOCAL_DATA_PATH / f"api/{season}/players/{element}.csv"
         path.parent.mkdir(parents=True, exist_ok=True)
-
-        # Save the data in CSV format
         pd.DataFrame(data).to_csv(path, index=False)
-
-    # Keep track data updates
-    with open(checkpoint, 'w') as f:
-        json.dump(last_updated_gameweek, f)
 
 
 def update_local_teams(season, teams):
--- datautil/updating/pipeline.py
+++ datautil/updating/pipeline.py
@@ -1,12 +1,27 @@
+import json
+
 from .understat.localdata import save_understat_league_dates_data
 from .understat.localdata import save_understat_league_teams_data
 from .understat.localdata import save_understat_player_matches_data
 from .understat.mappings import update_fixture_ids, update_player_ids, update_team_ids
 from .fpl import update_local_players, update_local_elements, update_local_teams, update_local_fixtures
 
+from datautil.constants import LOCAL_DATA_PATH
+
 
 def update_local_data(season, elements, events, teams):
-    update_local_players(season, elements, events)
+    """Update local data if it is out of date."""
+
+    last_updated_gameweek = int(events[events['data_checked'] == True]['id'].max())
+
+    # Check if the local data is already up to date
+    checkpoint = LOCAL_DATA_PATH / f"api/{season}/local_players_last_update.json"
+    if checkpoint.exists():
+        with open(checkpoint, 'r') as f:
+            if last_updated_gameweek == json.load(f):
+                return
+
+    update_local_players(season, elements)
     update_local_elements(season, elements)
     update_local_teams(season, teams)
     update_local_fixtures(season)
@@ -16,3 +31,8 @@ def update_local_data(season, elements, events, teams):
     update_player_ids(season)
     update_team_ids(season)
     update_fixture_ids(season)
+
+    # Keep track of the last updated gameweek
+    checkpoint.parent.mkdir(parents=True, exist_ok=True)
+    with open(checkpoint, 'w') as f:
+        json.dump(last_updated_gameweek, f)
