98e86f6fa8e5f17684faa1e2315b417bc714bdc3
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-05-13 22:05:25 -0400

    Add tests for understat loading

--- datautil/load/understat.py
+++ datautil/load/understat.py
@@ -8,21 +8,21 @@ def load_understat(seasons: list[str]):
     """Loads local understat data for the given seasons."""
 
     # Load local data
-    players = load_player_matches(seasons)
+    players = load_players(seasons)
     teams = load_teams(seasons)
-    dates = load_dates(seasons)
+    fixtures = load_fixtures(seasons)
     player_ids = load_player_ids()
     fixture_ids = load_fixture_ids(seasons)
     team_ids = load_team_ids()
 
-    # Add FPL seasons
+    # Convert understat seasons to FPL seasons
     players = players.with_columns(
         convert_year_col_to_season_col("season").alias("fpl_season")
     )
     teams = teams.with_columns(
         convert_year_col_to_season_col("season").alias("fpl_season")
     )
-    dates = dates.with_columns(
+    fixtures = fixtures.with_columns(
         convert_year_col_to_season_col("season").alias("fpl_season")
     )
     fixture_ids = fixture_ids.with_columns(
@@ -31,10 +31,9 @@ def load_understat(seasons: list[str]):
 
     # Add FPL player codes to players
     players = players.join(
-        player_ids.select(["understat_id", "fpl_code"]),
+        player_ids.select([pl.col("understat_id").alias("id"), pl.col("fpl_code")]),
         how="left",
-        left_on=["player_id"],
-        right_on=["understat_id"],
+        on="id",
     )
 
     # Add FPL fixture IDs to players
@@ -42,19 +41,18 @@ def load_understat(seasons: list[str]):
         fixture_ids.select(
             [
                 pl.col("fpl_season"),
-                pl.col("understat_id"),
+                pl.col("understat_id").alias("fixture_id"),
                 pl.col("fpl_id").alias("fpl_fixture_id"),
             ]
         ),
         how="left",
-        left_on=["fpl_season", "id"],
-        right_on=["fpl_season", "understat_id"],
+        on=["fpl_season", "fixture_id"],
     )
 
     # Any unmapped fixtures are for matches outside the EPL. Remove them.
     players = players.filter(pl.col("fpl_fixture_id").is_not_null())
 
-    # Add PPDA stats to teams
+    # Extract PPDA stats and add to teams
     pattern = r"\{'att':\s*(\d+),\s*'def':\s*(\d+)\}"
     teams = teams.with_columns(
         pl.col("ppda").str.extract(pattern, 1).cast(pl.Int32).alias("ppda_att"),
@@ -71,20 +69,26 @@ def load_understat(seasons: list[str]):
 
     # Add understat fixture IDs to teams
     teams = teams.join(
-        dates.select(
-            [pl.col("id").alias("fixture_id_h"), pl.col("h"), pl.col("datetime")]
+        fixtures.select(
+            [
+                pl.col("h").alias("id"),
+                pl.col("datetime").alias("date"),
+                pl.col("id").alias("fixture_id_h"),
+            ]
         ),
         how="left",
-        left_on=["id", "date"],
-        right_on=["h", "datetime"],
+        on=["id", "date"],
     )
     teams = teams.join(
-        dates.select(
-            [pl.col("id").alias("fixture_id_a"), pl.col("a"), pl.col("datetime")]
+        fixtures.select(
+            [
+                pl.col("a").alias("id"),
+                pl.col("datetime").alias("date"),
+                pl.col("id").alias("fixture_id_a"),
+            ]
         ),
         how="left",
-        left_on=["id", "date"],
-        right_on=["a", "datetime"],
+        on=["id", "date"],
     )
     teams = teams.with_columns(
         pl.when(pl.col("h_a") == "h")
@@ -99,27 +103,30 @@ def load_understat(seasons: list[str]):
         fixture_ids.select(
             [
                 pl.col("fpl_season"),
-                pl.col("understat_id"),
+                pl.col("understat_id").alias("fixture_id"),
                 pl.col("fpl_id").alias("fpl_fixture_id"),
             ]
         ),
         how="left",
-        left_on=["fpl_season", "fixture_id"],
-        right_on=["fpl_season", "understat_id"],
+        on=["fpl_season", "fixture_id"],
     )
 
     # Add FPL team codes to teams
     teams = teams.join(
-        team_ids.select(["understat_id", "fpl_code"]),
+        team_ids.select(
+            [
+                pl.col("understat_id").alias("id"),
+                pl.col("fpl_code"),
+            ]
+        ),
         how="left",
-        left_on=["id"],
-        right_on=["understat_id"],
+        on="id",
     )
 
     return players, teams
 
 
-def load_player_matches(seasons: list[str]) -> pl.LazyFrame:
+def load_players(seasons: list[str]) -> pl.LazyFrame:
     """Load player match data."""
     seasons = list(map(convert_season_to_year, seasons))
     return (
@@ -130,11 +137,12 @@ def load_player_matches(seasons: list[str]) -> pl.LazyFrame:
             include_file_paths="file_path",
         )
         .with_columns(
-            pl.col("file_path")
-            .str.extract(r"(\d+)\.csv")
-            .cast(pl.Int32)
-            .alias("player_id"),
+            # Rename the fixture ID column to avoid confusion
+            pl.col("id").alias("fixture_id"),
+            # Extract player ID from file path
+            pl.col("file_path").str.extract(r"(\d+)\.csv").cast(pl.Int32).alias("id"),
         )
+        .drop("file_path")
         .filter(pl.col("season").is_in(seasons))
     )
 
@@ -155,8 +163,8 @@ def load_teams(seasons: list[str]) -> pl.LazyFrame:
     return pl.concat(frames, how="diagonal")
 
 
-def load_dates(seasons: list[str]) -> pl.LazyFrame:
-    """Load date data."""
+def load_fixtures(seasons: list[str]) -> pl.LazyFrame:
+    """Load fixture data."""
     seasons = list(map(convert_season_to_year, seasons))
     frames = [
         pl.scan_csv(
--- tests/test_datautil.py
+++ tests/test_datautil.py
@@ -2,6 +2,7 @@ import polars as pl
 from polars.testing import assert_frame_equal
 
 from datautil.load.fpl import load_fpl
+from datautil.load.understat import load_understat
 
 
 def test_load_fpl():
@@ -43,6 +44,82 @@ def test_load_fpl():
                 pl.col("element_type"),
                 pl.col("team_code"),
                 pl.col("opponent_team_code"),
+            ]
+        )
+        .null_count()
+        .sum_horizontal()
+        .item()
+        == 0
+    )
+
+
+def test_load_understat():
+    # Load data for the 2022-23 season
+    seasons = ["2021-22"]
+    players, teams = load_understat(seasons)
+    players = players.collect()
+    teams = teams.collect()
+
+    # Test that the correct number of rows is returned
+    assert players.filter(pl.col("id") == 453).height == 35
+    assert players.filter(pl.col("id") == 447).height == 30
+    assert teams.filter(pl.col("id") == 88).height == 38
+    assert teams.filter(pl.col("id") == 82).height == 38
+
+    # Test that player mappings are correct
+    expected = pl.DataFrame(
+        {
+            "season": [2021, 2021],
+            "id": [453, 447],
+            "fpl_code": [85971, 61366],
+            "fixture_id": [16385, 16385],
+            "fpl_fixture_id": [10, 10],
+        }
+    )
+    result = players.join(
+        expected.select(["season", "id", "fpl_fixture_id"]),
+        on=["season", "id", "fpl_fixture_id"],
+        how="inner",
+    ).select(expected.columns)
+    assert_frame_equal(
+        result,
+        expected,
+        check_row_order=False,
+        check_column_order=False,
+        check_exact=True,
+        check_dtypes=False,
+    )
+
+    # Test that team mappings are correct
+    expected = pl.DataFrame(
+        {
+            "season": [2021, 2021],
+            "id": [88, 82],
+            "fpl_code": [43, 6],
+            "fixture_id": [16385, 16385],
+            "fpl_fixture_id": [10, 10],
+        }
+    )
+    result = teams.join(
+        expected.select(["season", "id", "fpl_fixture_id"]),
+        on=["season", "id", "fpl_fixture_id"],
+        how="inner",
+    ).select(expected.columns)
+    assert_frame_equal(
+        result,
+        expected,
+        check_row_order=False,
+        check_column_order=False,
+        check_exact=True,
+        check_dtypes=False,
+    )
+
+    # Test that mappings leave no null values
+    assert (
+        players.select(
+            [
+                pl.col("fpl_code"),
+                pl.col("fpl_fixture_id"),
             ]
         )
         .null_count()
