cf82478ff08ae35b8de0148336c430f6294c460d
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-05-14 22:59:03 -0400

    Rename `ROOT_DIR` to `MODELS_DIR` in model.py

--- prediction/model.py
+++ prediction/model.py
@@ -3,20 +3,20 @@ from pathlib import Path
 
 from sklearn.base import BaseEstimator
 
-ROOT_DIR = Path("models")
+MODELS_DIR = Path("models")
 
 
 def save_model(model: BaseEstimator, name: str):
     """Save the model to a file."""
-    ROOT_DIR.mkdir(parents=True, exist_ok=True)
-    path = ROOT_DIR / f"{name}.pkl"
+    MODELS_DIR.mkdir(parents=True, exist_ok=True)
+    path = MODELS_DIR / f"{name}.pkl"
     with open(path, "wb") as f:
         pickle.dump(model, f)
 
 
 def load_model(name: str) -> BaseEstimator:
     """Load the model from a file."""
-    path = ROOT_DIR / f"{name}.pkl"
+    path = MODELS_DIR / f"{name}.pkl"
     if not path.exists():
         raise FileNotFoundError(f"Model {name} not found at {path}.")
     with open(path, "rb") as f:
