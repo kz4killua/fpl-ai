0996aab304a8a932bde91cbe55de087143366940
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-07-09 15:03:45 -0400

    Add command for model training

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import argparse
+import sys
+import warnings
 
 from optimization.tune import tune
+from prediction.train import train
 from simulation.simulate import simulate
 
 
 def main():
+    sys.stdout.reconfigure(encoding='utf-8')
+    warnings.filterwarnings("ignore", category=UserWarning)
+
     # Set up command-line argument parsing
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest="command", help="Commands")
     simulate_parser = subparsers.add_parser("simulate", help="Simulate seasons")
     _ = subparsers.add_parser("tune", help="Tune hyperparameters")
+    _ = subparsers.add_parser("train", help="Train models")
+
     simulate_parser.add_argument(
         "--season",
         type=str,
@@ -19,15 +27,17 @@ def main():
     simulate_parser.add_argument(
         "--log", action="store_true", help="Log simulation details"
     )
-    args = parser.parse_args()
 
     # Execute the appropriate command based on user input
+    args = parser.parse_args()
     if args.command == "tune":
         tune()
     elif args.command == "simulate":
-        # Simulate the season
         points = simulate(args.season, [], log=args.log)
         print(f"{args.season}: {points} points")
+    elif args.command == "train":
+        train()
+        print("Models trained successfully.")
     else:
         parser.print_help()
 
---
+++ prediction/train.py
@@ -0,0 +1,36 @@
+import polars as pl
+
+from datautil.load.merged import load_merged
+from datautil.utils import get_seasons
+from features.engineer_features import (
+    engineer_match_features,
+    engineer_player_features,
+    engineer_team_features,
+)
+from prediction.model import PredictionModel
+from prediction.utils import save_model
+
+
+def train():
+
+    # Load player and match data
+    seasons = get_seasons("2023-24")
+    players, teams, managers = load_merged(seasons)
+    players = players.collect()
+    teams = teams.collect()
+    managers = managers.collect()
+
+    # Engineer features for prediction
+    players = engineer_player_features(players)
+    teams = engineer_team_features(teams)
+    matches = engineer_match_features(teams)
+
+    # Fit and save models for simulations
+    seasons = ["2021-22", "2022-23", "2023-24"]
+    for season in seasons:
+        # Only train on seasons before the current one
+        players_season = players.filter(pl.col("season") < season)
+        matches_season = matches.filter(pl.col("season") < season)
+        model = PredictionModel()
+        model.fit(players_season, matches_season)
+        save_model(model, f"simulation_{season}")
