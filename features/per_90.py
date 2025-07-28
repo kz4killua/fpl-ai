d1ce93e312a11700839ae26fac5bcc2e40e0872d
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-07-28 16:13:31 -0400

    Handle null values in per-90 calculations

--- features/per_90.py
+++ features/per_90.py
@@ -5,9 +5,12 @@ def compute_per_90(df: pl.LazyFrame, columns: list[str]) -> pl.LazyFrame:
     """Compute per 90 statistics for players."""
     df = df.with_columns(
         [
-            pl.when(pl.col("minutes") > 0)
-            .then((pl.col(column) / pl.col("minutes") * 90).alias(f"{column}_per_90"))
-            .otherwise(0)
+            pl.when(pl.col("minutes").is_null())
+            .then(None)
+            .when(pl.col("minutes") == 0)
+            .then(0)
+            .otherwise(pl.col(column) / pl.col("minutes") * 90)
+            .alias(f"{column}_per_90")
             for column in columns
         ]
     )
