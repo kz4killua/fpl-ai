5dec04ee9df24d6e881f78781bfb405a2cba8664
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-07-28 16:47:34 -0400

    Update the training function to include latest seasons

--- prediction/train.py
+++ prediction/train.py
@@ -13,7 +13,7 @@ from prediction.utils import save_model
 
 def train():
     # Load player and match data
-    seasons = get_seasons("2023-24")
+    seasons = get_seasons("2024-25")
     players, teams, managers = load_merged(seasons)
     players = players.collect()
     teams = teams.collect()
@@ -25,11 +25,18 @@ def train():
     matches = engineer_match_features(teams)
 
     # Fit and save models for simulations
-    seasons = ["2021-22", "2022-23", "2023-24"]
     for season in seasons:
+        if season < "2021-22":
+            continue
+
         # Only train on seasons before the current one
         players_season = players.filter(pl.col("season") < season)
         matches_season = matches.filter(pl.col("season") < season)
         model = PredictionModel()
         model.fit(players_season, matches_season)
         save_model(model, f"simulation_{season}")
+
+    # Fit a final model on all data
+    model = PredictionModel()
+    model.fit(players, matches)
+    save_model(model, "live")
