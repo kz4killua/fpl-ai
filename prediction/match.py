9978ad4c6db99d280d02ca1cedc43e2637028bf8
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-05 21:25:38 -0400

    Update team goals scored predictor

--- prediction/match.py
+++ prediction/match.py
@@ -1,7 +1,7 @@
 from sklearn.impute import KNNImputer
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import PoissonRegressor
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 from prediction.utils import FeatureSelector
@@ -9,22 +9,35 @@ from prediction.utils import FeatureSelector
 
 def make_match_predictor():
     columns = [
-        "team_h_relative_attack_strength",
-        "team_a_relative_attack_strength",
+        "team_h_clb_elo",
+        "team_a_clb_elo",
+        "team_h_clb_elo_win_probability",
+        "team_a_clb_elo_win_probability",
+        "team_h_relative_uds_xG_rolling_mean_5",
+        "team_a_relative_uds_xG_rolling_mean_5",
         "team_h_relative_uds_xG_rolling_mean_10",
         "team_a_relative_uds_xG_rolling_mean_10",
-        "team_h_relative_uds_xG_rolling_mean_30",
-        "team_a_relative_uds_xG_rolling_mean_30",
+        "team_h_relative_uds_xG_rolling_mean_20",
+        "team_a_relative_uds_xG_rolling_mean_20",
+        "team_h_relative_uds_xG_rolling_mean_40",
+        "team_a_relative_uds_xG_rolling_mean_40",
+        "team_h_relative_scored_rolling_mean_5",
+        "team_a_relative_scored_rolling_mean_5",
         "team_h_relative_scored_rolling_mean_10",
         "team_a_relative_scored_rolling_mean_10",
-        "team_h_relative_scored_rolling_mean_30",
-        "team_a_relative_scored_rolling_mean_30",
+        "team_h_relative_scored_rolling_mean_20",
+        "team_a_relative_scored_rolling_mean_20",
+        "team_h_relative_scored_rolling_mean_40",
+        "team_a_relative_scored_rolling_mean_40",
     ]
-    return make_pipeline(
-        FeatureSelector(columns),
-        KNNImputer(n_neighbors=5),
-        StandardScaler(),
-        MultiOutputRegressor(
-            Ridge(alpha=100),
-        ),
+    model = MultiOutputRegressor(
+        PoissonRegressor(alpha=1),
+    )
+    return Pipeline(
+        [
+            ("features", FeatureSelector(columns)),
+            ("imputer", KNNImputer(n_neighbors=5)),
+            ("scaler", StandardScaler()),
+            ("predictor", model),
+        ]
     )
