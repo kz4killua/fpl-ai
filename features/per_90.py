97b5cea5a4272547745545ad8669205f9e5402f1
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-10 16:01:23 -0400

    Use `None` for per-90 when minutes is `0`

--- features/per_90.py
+++ features/per_90.py
@@ -8,7 +8,7 @@ def compute_per_90(df: pl.LazyFrame, columns: list[str]) -> pl.LazyFrame:
             pl.when(pl.col("minutes").is_null())
             .then(None)
             .when(pl.col("minutes") == 0)
-            .then(0)
+            .then(None)
             .otherwise(pl.col(column) / pl.col("minutes") * 90)
             .alias(f"{column}_per_90")
             for column in columns
