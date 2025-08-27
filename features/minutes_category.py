4a446b3ede6bf32973939351deac0d7fbfa8fd22
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-27 03:32:13 -0400

    Fix bug with minute categories

--- features/minutes_category.py
+++ features/minutes_category.py
@@ -4,7 +4,9 @@ import polars as pl
 def compute_minutes_category(df: pl.LazyFrame):
     """Categorize players based on their minutes played."""
     return df.with_columns(
-        pl.when(pl.col("minutes") == 0)
+        pl.when(pl.col("minutes").is_null())
+        .then(pl.lit(None))
+        .when(pl.col("minutes").eq(0))
         .then(pl.lit("0_minutes"))
         .when(pl.col("minutes").is_between(1, 59))
         .then(pl.lit("1_to_59_minutes"))
