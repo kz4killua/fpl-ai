fbec334113bf24c74596142c5a94379bd7902f09
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-08-27 03:52:23 -0400

    Fix bug in `compute_one_hot_minutes_category`

--- features/one_hot_minutes_category.py
+++ features/one_hot_minutes_category.py
@@ -3,4 +3,18 @@ import polars as pl
 
 def compute_one_hot_minutes_category(df: pl.DataFrame):
     """Compute one-hot encoded categories for minute categories."""
-    return df.with_columns(df.select("minutes_category").to_dummies())
+    values = [
+        "0_minutes",
+        "1_to_59_minutes",
+        "60_plus_minutes",
+    ]
+    for value in values:
+        df = df.with_columns(
+            pl.when(pl.col("minutes_category").is_null())
+            .then(pl.lit(None))
+            .when(pl.col("minutes_category") == value)
+            .then(pl.lit(1))
+            .otherwise(pl.lit(0))
+            .alias(f"minutes_category_{value}"),
+        )
+    return df
