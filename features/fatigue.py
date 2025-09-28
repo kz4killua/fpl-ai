439b7e4130533060e8572bb36911ee5eabd2477f
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-09-27 23:41:29 -0400

    Fix bug in `compute_fatigue`

--- features/fatigue.py
+++ features/fatigue.py
@@ -4,21 +4,22 @@ import polars as pl
 def compute_fatigue(df: pl.DataFrame, window: int):
     """Compute the total number of minutes played in the last N days."""
 
-    # Sort by kickoff time (if necessary).
+    # Sort by kickoff time if necessary
     if not df.get_column("kickoff_time").is_sorted():
         df = df.sort("kickoff_time")
 
-    # Compute player fatigue over the given number of days
+    # Compute rolling minutes over the given number of days
     alias = f"minutes_sum_{window}_days"
-    fatigue = (
-        df.rolling(
-            "kickoff_time",
-            period=f"{window}d",
-            closed="left",
-            group_by=("season", "code"),
-        )
-        .agg([pl.sum("minutes").alias(alias)])
-        .get_column(alias)
-    )
+    fatigue = df.rolling(
+        index_column="kickoff_time",
+        period=f"{window}d",
+        closed="left",
+        group_by=("season", "code"),
+    ).agg(pl.sum("minutes").alias(alias))
 
-    return df.with_columns(fatigue)
+    # Note: A join ensures that the computed values align with the original DataFrame
+    return df.join(
+        fatigue.select("season", "code", "kickoff_time", alias),
+        on=("season", "code", "kickoff_time"),
+        how="left",
+    )
