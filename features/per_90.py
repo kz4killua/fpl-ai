ae994bcf31ce44cf2305905297769f20f6051f9c
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-10-18 12:08:50 -0400

    Add `threshold` to `compute_per_90`

--- features/per_90.py
+++ features/per_90.py
@@ -1,13 +1,17 @@
 import polars as pl
 
 
-def compute_per_90(df: pl.LazyFrame, columns: list[str]) -> pl.LazyFrame:
-    """Compute per 90 statistics for players."""
+def compute_per_90(
+    df: pl.LazyFrame, columns: list[str], threshold: int = 30
+) -> pl.LazyFrame:
+    """Compute per-90 statistics for players with minutes above the threshold."""
     df = df.with_columns(
         [
             pl.when(pl.col("minutes").is_null())
             .then(None)
             .when(pl.col("minutes") == 0)
+            .then(None)
+            .when(pl.col("minutes") < threshold)
             .then(None)
             .otherwise(pl.col(column) / pl.col("minutes") * 90)
             .alias(f"{column}_per_90")
