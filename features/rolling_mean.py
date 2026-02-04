c7e51ee5e0e8464193152dadb9214d72212f9d89
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2026-02-04 06:48:52 -0500

    Revert changes to rolling calculations

--- features/rolling_mean.py
+++ features/rolling_mean.py
@@ -4,43 +4,49 @@ import polars as pl
 def compute_rolling_mean(
     df: pl.LazyFrame,
     columns: list[str],
-    windows: list[int],
+    window_sizes: list[int],
     over: list[str] = ("season", "code"),
     condition: pl.Expr | None = None,
     suffix: str = "",
 ) -> pl.LazyFrame:
     """Calculate rolling means ignoring null values."""
+
+    # Sort rows for forward-filling
+    df = df.sort("kickoff_time")
+
+    # Add a temporary index column (to skip null values)
+    df = df.with_row_index("index")
+
     if condition is None:
         condition = pl.lit(True)
 
-    df = df.sort("kickoff_time").with_row_index("index")
-
-    # Filter out invalid rows. We only do this once for efficiency.
-    has_valid_data = pl.any_horizontal([pl.col(c).is_not_null() for c in set(columns)])
-    subset = df.filter(condition & has_valid_data)
-
-    # Create expressions for computing rolling means
-    rolling_mean_expressions = []
-    resulting_columns = []
-
-    for c, w in zip(columns, windows, strict=True):
-        alias = f"{c}_rolling_mean_{w}{suffix}"
-        rolling_mean_expressions.append(
-            pl.col(c).rolling_mean(window_size=w, min_periods=1).over(over).alias(alias)
+    for column, window_size in zip(columns, window_sizes, strict=True):
+        alias = f"{column}_rolling_mean_{window_size}{suffix}"
+        # Compute the rolling mean for selected values
+        selected = df.filter(pl.col(column).is_not_null() & condition).select(
+            ["index", *over, column]
         )
-        resulting_columns.append(alias)
+        selected = selected.with_columns(
+            pl.col(column)
+            .rolling_mean(window_size, min_samples=1)
+            .over(over)
+            .alias(alias)
+        )
+        # Add the selected results to the original frame
+        df = df.join(
+            selected.select(["index", alias]),
+            on="index",
+            how="left",
+        )
+        df = df.with_columns(
+            pl.col(alias)
+            # Important: To avoid data leakage, shift the rolling mean by 1
+            .shift(1)
+            .forward_fill()
+            .over(over)
+        )
 
-    # Compute all rolling means in one go
-    subset = subset.with_columns(rolling_mean_expressions)
-
-    # Join back to the original dataframe
-    df = df.join(subset.select(["index"] + resulting_columns), on="index", how="left")
-
-    # Shift and forward fill to avoid data leakage
-    fill_expressions = []
-    for c in resulting_columns:
-        fill_expressions.append(pl.col(c).shift(1).forward_fill().over(over))
-
-    df = df.with_columns(fill_expressions).drop("index")
+    # Drop the temporary index column
+    df = df.drop("index")
 
     return df
